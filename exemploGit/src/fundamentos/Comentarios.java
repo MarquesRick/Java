55c835a43999dce80b11a1454d94e0da5ecc8b13
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-03-16 13:42:11 -0300

    ok

---
+++ exemploGit/src/fundamentos/Comentarios.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class Comentarios {
+	public static void main(String[] args) {
+		System.out.println("Primeiro");
+		System.out.println("Segundo");
+		System.out.println("Terceiro");
+	}
+}
