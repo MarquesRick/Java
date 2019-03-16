da0334165f1a7708554ed9cf55d3a0eeae03ca01
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2019-03-16 01:50:02 -0300

    Update exemploGitClass.java

--- exemploGit/src/exemploGit/exemploGitClass.java
+++ exemploGit/src/exemploGit/exemploGitClass.java
@@ -3,8 +3,8 @@ package exemploGit;
 public class exemploGitClass {
 	
 	public static void main(String[] args){
-		System	.out.println("Teste");
-		System	.out.println("Teste Pull");
+		System.out.println("Teste");
+		System.out.println("Teste Pull");
 
 	}
 
