b6b347f18cb52dc655d7cc9fc5f887eecc457775
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-03-16 12:56:34 -0300

    Primeiro Programa

--- exemploGit/src/exemploGit/exemploGitClass.java
+++ exemploGit/src/exemploGit/exemploGitClass.java
@@ -1,8 +1,8 @@
 package exemploGit;
 
 public class exemploGitClass {
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		System.out.println("Teste");
 		System.out.println("Teste Pull");
 		System.out.println("Renomeando projeto");
