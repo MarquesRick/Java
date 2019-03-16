e34d4ee2c2956308ca61f11d769d12824a57245d
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-03-16 13:42:26 -0300

    ok

--- exemploGit/src/fundamentos/Comentarios.java
+++ exemploGit/src/fundamentos/Comentarios.java
@@ -1,8 +1,24 @@
 package fundamentos;
-
+/**
+ * Classe reponsável por demonstrar os 3 tipos de comentários no JAVA
+ * @author HENRIQUEMARQUES [henrique.marques2@fatec.sp.gov.br]
+ * @version 1.0
+ * @since 2.2 
+ */
 public class Comentarios {
+	
+	/**
+	 * Esse método é a porta de entrada de um programa Java
+	 * @param args
+	 * Parametros passados por linha de comando
+	 */
+	
 	public static void main(String[] args) {
+		/*
+		 *Imprime no console a palavra 'primeiro'
+		 */
 		System.out.println("Primeiro");
+		//Cuidado com  os comentários óbvios
 		System.out.println("Segundo");
 		System.out.println("Terceiro");
 	}
