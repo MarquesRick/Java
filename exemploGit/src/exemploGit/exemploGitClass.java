3828625d6343067de1b656361b1855c01f2eef61
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-03-16 13:12:10 -0300

    ok

---
+++ exemploGit/src/exemploGit/exemploGitClass.java
@@ -0,0 +1,12 @@
+package exemploGit;
+
+public class exemploGitClass {
+	
+	public static void main(String[] args){
+		System.out.println("Teste");
+		System.out.println("Teste Pull");
+		System.out.println("Renomeando projeto");
+
+	}
+
+}
