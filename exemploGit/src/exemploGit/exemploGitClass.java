7b2faaed7174f12ea2dc9bb44ed539512a9d5422
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-03-16 01:56:09 -0300

    renomeação

--- exemploGit/src/exemploGit/exemploGitClass.java
+++ exemploGit/src/exemploGit/exemploGitClass.java
@@ -5,6 +5,7 @@ public class exemploGitClass {
 	public static void main(String[] args){
 		System.out.println("Teste");
 		System.out.println("Teste Pull");
+		System.out.println("Renomeando projeto");
 
 	}
 
