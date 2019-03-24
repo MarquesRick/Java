ff8d261ebd098387deeb574bd030b9a3c545ea6c
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-03-24 13:28:05 -0300

    ok

---
+++ exemploGit/src/fundamentos/OperadoresLogicos.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class OperadoresLogicos {
+	public static void main(String[] args) {
+		boolean executouTrabalho1 = false;
+		boolean executouTrabalho2 = true;
+		boolean comprouSorvete = executouTrabalho1 || executouTrabalho2;
+		boolean comprouTv50 = executouTrabalho1 && executouTrabalho2;
+		boolean comprouTv32 = executouTrabalho1 ^ executouTrabalho2; //"Ou Exclusivo" "^";
+		
+		System.out.println("Sorvete = " + comprouSorvete);
+		System.out.println("TV 50 = " + comprouTv50);
+		System.out.println("TV 32 = " + comprouTv32);
+		//Operador Unário intruso 
+		System.out.println("Fome = " + !comprouSorvete);
+	}
+}
