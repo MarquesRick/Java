5f9a7f2cdf1afe849005111b64622608ccf4c5ce
Henrique Marques <MarquesRick@users.noreply.github.com>
2019-03-24 23:59:54 -0300

    Estrutura de Controle.

---
+++ exemploGit/src/controle/IfElse.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class IfElse {
+public static void main(String[] args) {
+	double nota = 7;
+	
+	if(nota >= 7)
+		System.out.println("Aprovado!\n" + "Parabéns!");
+	else
+		System.out.println("Reprovado");
+}
+}
